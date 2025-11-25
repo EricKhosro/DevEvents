04164b16769f005c2dd88223302b09ef754b9adf
Eric Khosrafian <erickhosrafian@gmail.com>
2025-11-25 11:35:15 +0400

    modify folder structure

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         </Link>
         <ul>
           <Link href="/">Home</Link>
-          <Link href="/create">Create Event</Link>
+          <Link href="/events/create">Create Event</Link>
         </ul>
       </nav>
     </header>
